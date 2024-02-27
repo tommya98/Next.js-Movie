48e03d4f91d5e9a7db825e0fef01c42e0a4fd5e3
tommya98 <tommya98@naver.com>
2024-02-27 15:51:29 +0900

    FEAT: Movie 컴포넌트 구현

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { BASE_URL } from "../constant";
+import Movie from "@/components/Movie";
 
 interface IMovie {
   adult: boolean;
@@ -31,14 +32,15 @@ async function getMovies() {
 export default async function Home() {
   const movies = await getMovies();
   return (
-    <div>
-      <ul className="grid grid-cols-5 gap-6 mx-auto max-w-[90%] w-full">
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
-          </li>
-        ))}
-      </ul>
+    <div className="grid grid-cols-5 gap-6 mx-auto max-w-[90%] w-full">
+      {movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          poster_path={movie.poster_path}
+          title={movie.title}
+        ></Movie>
+      ))}
     </div>
   );
 }
