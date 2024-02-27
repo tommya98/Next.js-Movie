9d224ad8efcdb5a90c0d0de3bbccb0040ec8ecd9
tommya98 <tommya98@naver.com>
2024-02-27 13:36:14 +0900

    FEAT: 에러 핸들링 구현

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Link from "next/link";
+
+export default function ErrorOMG() {
+  return (
+    <>
+      <h1>Error</h1>
+      <Link href="/">Return Home</Link>
+    </>
+  );
+}
