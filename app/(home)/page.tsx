8d2faf2b24ce4ad1957c7419e7cbbc89aa147da6
tommya98 <tommya98@naver.com>
2024-02-27 13:33:30 +0900

    FEAT: 영화 디테일 fetch 구현

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,4 +1,22 @@
+import Link from "next/link";
 import { BASE_URL } from "../constant";
+
+interface IMovie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
 
 export const metadata = {
   title: "Home",
@@ -6,12 +24,22 @@ export const metadata = {
 
 async function getMovies() {
   const response = await fetch(BASE_URL);
-  const json = await response.json();
-  const data = JSON.stringify(json);
-  return data;
+  const json: IMovie[] = await response.json();
+  return json;
 }
 
 export default async function Home() {
   const movies = await getMovies();
-  return <div>{movies}</div>;
+  return (
+    <div>
+      <h1>All Movies</h1>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
---
+++ components/MovieVideos.tsx
@@ -0,0 +1,11 @@
+import { BASE_URL } from "@/app/constant";
+
+async function getVideos(id: string) {
+  const response = await fetch(`${BASE_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieVideos({ id }: { id: string }) {
+  const videos = await getVideos(id);
+  return <h6>{JSON.stringify(videos)}</h6>;
+}
