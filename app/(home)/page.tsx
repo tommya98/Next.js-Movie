995b9b1599c3857f1d939f08903c3291e942cb51
tommya98 <tommya98@naver.com>
2024-02-27 13:04:27 +0900

    FEAT: 기본 영화 데이터 fetch 구현

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,17 @@
+import { BASE_URL } from "../constant";
+
 export const metadata = {
   title: "Home",
 };
 
-export default function Home() {
-  return <div>Hello</div>;
+async function getMovies() {
+  const response = await fetch(BASE_URL);
+  const json = await response.json();
+  const data = JSON.stringify(json);
+  return data;
+}
+
+export default async function Home() {
+  const movies = await getMovies();
+  return <div>{movies}</div>;
 }
