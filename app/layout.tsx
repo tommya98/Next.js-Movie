7589a3af53550429e2ee00bffc913053cb3569cf
tommya98 <tommya98@naver.com>
2024-02-27 11:59:35 +0900

    FEAT: Navigation 컴포넌트 구현

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Navigation from "@/components/Navagation";
 
 export const metadata: Metadata = {
   title: "MOIVES",
@@ -12,7 +13,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navigation />
+        {children}
+      </body>
     </html>
   );
 }
