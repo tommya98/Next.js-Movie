fd257b2e0595d2a0bef8be9ae58be5d87a9ed2ba
tommya98 <tommya98@naver.com>
2024-02-28 15:45:28 +0900

    STYLE: 유튜브 영상 크기 조절

--- components/MovieVidoes/MovieVideos.tsx
+++ components/MovieVidoes/MovieVideos.tsx
@@ -6,14 +6,17 @@ export default async function MovieVideos({ id }: { id: string }) {
   return (
     <div className="container w-4/5 mx-auto grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-10 mt-10 mb-10">
       {videos.map((video) => (
-        <iframe
-          key={video.id}
-          src={`https://youtube.com/embed/${video.key}`}
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          title={video.name}
-          className="rounded-md opacity-80 transition-opacity duration-200 ease-in-out hover:opacity-100"
-        />
+        <div key={video.id} className="flex justify-center items-center">
+          {" "}
+          {/* Added wrapper div for centering */}
+          <iframe
+            src={`https://youtube.com/embed/${video.key}`}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title={video.name}
+            className="rounded-md opacity-80 transition-opacity duration-200 ease-in-out hover:opacity-100 h-60 w-auto"
+          />
+        </div>
       ))}
     </div>
   );
