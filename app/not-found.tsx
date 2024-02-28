5fe44cf7accdab8458aceeca1eff4dc9edd16cb9
tommya98 <tommya98@naver.com>
2024-02-28 15:38:08 +0900

    STYLE: NotFound 스타일링

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,10 +6,17 @@ export const metadata = {
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-col items-center justify-center ">
+      <h2 className="text-3xl font-bold text-red-600 mb-4">404 - Not Found</h2>
+      <p className="text-lg  mb-5">
+        Sorry, we could not find the requested resource.
+      </p>
+      <Link
+        href="/"
+        className="px-6 py-3 text-white bg-blue-500 rounded hover:bg-blue-700 transition-all duration-300"
+      >
+        Return Home
+      </Link>
     </div>
   );
 }
