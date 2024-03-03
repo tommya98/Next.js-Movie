4290465699d2818b3e54557e5d37382ace953e0f
tommya98 <tommya98@naver.com>
2024-03-03 16:22:06 +0900

    STYLE: 영화 비디오 스타일 수정

--- components/MovieVidoes/MovieVideos.tsx
+++ components/MovieVidoes/MovieVideos.tsx
@@ -4,17 +4,18 @@ export default async function MovieVideos({ id }: { id: string }) {
   const videos = await getVideos(id);
 
   return (
-    <div className="container w-4/5 mx-auto grid grid-cols-[repeat(auto-fit,_minmax(250px,_1fr))] gap-10 mt-10 mb-10">
+    <div className="container w-4/5 mx-auto grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-10 mt-10 mb-10">
       {videos.map((video) => (
-        <div key={video.id} className="flex justify-center items-center">
-          {" "}
-          {/* Added wrapper div for centering */}
+        <div
+          key={video.id}
+          className="flex justify-center items-center w-max justify-self-center"
+        >
           <iframe
             src={`https://youtube.com/embed/${video.key}`}
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
             title={video.name}
-            className="rounded-md opacity-80 transition-opacity duration-200 ease-in-out hover:opacity-100 h-60 w-auto"
+            className="rounded-md opacity-80 transition-opacity duration-200 ease-in-out hover:opacity-100 h-60 w-full"
           />
         </div>
       ))}
