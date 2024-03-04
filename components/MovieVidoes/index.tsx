85cbe0f0e9adb2752db0304e761026ba9fe938a6
tommya98 <tommya98@naver.com>
2024-03-04 18:02:17 +0900

    FEAT: 영화 비디오 갯수 제한 적용

--- components/MovieVidoes/index.tsx
+++ components/MovieVidoes/index.tsx
@@ -5,7 +5,7 @@ export default async function MovieVideos({ id }: { id: string }) {
 
   return (
     <div className="container w-4/5 mx-auto grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-10 mt-t mb-10">
-      {videos.map((video) => (
+      {videos.splice(0, 12).map((video) => (
         <div
           key={video.id}
           className="flex justify-center items-center w-max justify-self-center"
